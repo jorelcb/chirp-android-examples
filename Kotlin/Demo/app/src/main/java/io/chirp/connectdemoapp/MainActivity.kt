25b59a0d316c0ead508e24db9efaf128c8f8b7dd
Joe Todd <joe@chirp.io>
2019-02-14 10:55:54 +0000

    Add Kotlin demo app

---
+++ Kotlin/Demo/app/src/main/java/io/chirp/connectdemoapp/MainActivity.kt
@@ -0,0 +1,249 @@
+package io.chirp.connectdemoapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.design.widget.Snackbar
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import io.chirp.connect.ChirpConnect
+import io.chirp.connect.models.ChirpConnectState
+
+private const val REQUEST_RECORD_AUDIO = 1
+internal var APP_KEY = ""
+internal var APP_SECRET = ""
+internal var APP_CONFIG = ""
+
+
+private const val TAG = "ConnectDemoApp"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var chirpConnect: ChirpConnect
+
+    private lateinit var status: TextView
+    private lateinit var lastChirp: TextView
+    private lateinit var versionView: TextView
+    private lateinit var startStopSdkBtn: Button
+    private lateinit var sendPayloadBtn: Button
+    private lateinit var parentLayout: View
+
+    private var startStopSdkBtnPressed: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.parentLayout = findViewById<View>(android.R.id.content)
+
+        this.status = findViewById(R.id.stateValue)
+        this.lastChirp = findViewById(R.id.lastChirp)
+        this.versionView = findViewById(R.id.versionView)
+        this.startStopSdkBtn = findViewById(R.id.startStopSdkBtn)
+        this.sendPayloadBtn = findViewById(R.id.sendPayloadBtn)
+
+        sendPayloadBtn.alpha = .4f
+        sendPayloadBtn.isClickable = false
+        sendPayloadBtn.setOnClickListener { sendPayload() }
+        startStopSdkBtn.alpha = .4f
+        startStopSdkBtn.isClickable = false
+        startStopSdkBtn.setOnClickListener { startStopSdk() }
+
+        if (APP_KEY == "" || APP_SECRET == "") {
+            Log.e(TAG, "APP_KEY or APP_SECRET is not set. " +
+                    "Please update with your APP_KEY/APP_SECRET from developers.chirp.io")
+            return
+        }
+
+        /**
+         * Key and secret initialisation
+         */
+        this.chirpConnect = ChirpConnect(this, APP_KEY, APP_SECRET)
+        Log.v(TAG, "Connect Version: " + chirpConnect.version)
+        versionView.text = chirpConnect.version
+        val setConfigError = chirpConnect.setConfig(APP_CONFIG)
+        if (setConfigError.code > 0) {
+            Log.e(TAG, setConfigError.message)
+            return
+        }
+        startStopSdkBtn.alpha = 1f
+        startStopSdkBtn.isClickable = true
+        val versionText = chirpConnect.version + "\n" +
+                chirpConnect.getProtocolName() + " v" +
+                chirpConnect.getProtocolVersion()
+        versionView.text = versionText
+
+        chirpConnect.onSent { payload: ByteArray, channel: Int ->
+            /**
+             * onSent is called when a send event has completed.
+             * The payload argument contains the payload data that was sent.
+             */
+            val hexData = chirpConnect.asHexString(payload)
+            updateLastPayload(hexData)
+            Log.v(TAG, "ConnectCallback: onSent: $hexData on channel: $channel")
+        }
+
+        chirpConnect.onReceived { payload: ByteArray?, channel: Int ->
+            /**
+             * onReceived is called when a receive event has completed.
+             * If the payload was decoded successfully, it is passed in payload.
+             * Otherwise, payload is null.
+             */
+            var hexData = "null"
+            if (payload != null) {
+                hexData = chirpConnect.asHexString(payload)
+            }
+            Log.v(TAG, "ConnectCallback: onReceived: $hexData on channel: $channel")
+            updateLastPayload(hexData)
+        }
+
+        chirpConnect.onSending { payload: ByteArray, channel: Int ->
+            /**
+             * onSending is called when a send event begins.
+             * The data argument contains the payload being sent.
+             */
+            val hexData = chirpConnect.asHexString(payload)
+            Log.v(TAG, "ConnectCallback: onSending: $hexData on channel: $channel")
+            updateLastPayload(hexData)
+        }
+
+        chirpConnect.onReceiving { channel: Int ->
+            /**
+             * onReceiving is called when a receive event begins.
+             * No data has yet been received.
+             */
+            Log.v(TAG, "ConnectCallback: onReceiving on channel: $channel")
+        }
+
+        chirpConnect.onStateChanged { oldState: ChirpConnectState, newState: ChirpConnectState ->
+            /**
+             * onStateChanged is called when the SDK changes state.
+             */
+            Log.v(TAG, "ConnectCallback: onStateChanged $oldState -> $newState")
+            when (newState) {
+                ChirpConnectState.CHIRP_CONNECT_STATE_NOT_CREATED -> updateStatus("NotCreated")
+                ChirpConnectState.CHIRP_CONNECT_STATE_STOPPED  -> updateStatus("Stopped")
+                ChirpConnectState.CHIRP_CONNECT_STATE_PAUSED  -> updateStatus("Paused")
+                ChirpConnectState.CHIRP_CONNECT_STATE_RUNNING  -> updateStatus("Running")
+                ChirpConnectState.CHIRP_CONNECT_STATE_SENDING  -> updateStatus("Sending")
+                ChirpConnectState.CHIRP_CONNECT_STATE_RECEIVING  -> updateStatus("Receiving")
+                else -> updateStatus(newState.toString())
+            }
+        }
+
+        chirpConnect.onSystemVolumeChanged { oldVolume: Int, newVolume: Int ->
+            /**
+             * onSystemVolumeChanged is called when the system volume is changed.
+             *
+            */
+            val snackBar = Snackbar.make(parentLayout, "System volume has been changed to: $newVolume", Snackbar.LENGTH_LONG)
+            snackBar.setAction("CLOSE") { }
+                    .setActionTextColor(ContextCompat.getColor(this, android.R.color.holo_red_light))
+                    .show()
+            Log.v(TAG, "System volume has been changed, notify user to increase the volume when sending data")
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_AUDIO)
+        } else {
+            if (startStopSdkBtnPressed) startSdk()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_RECORD_AUDIO -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (startStopSdkBtnPressed) stopSdk()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        chirpConnect.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            chirpConnect.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopSdk()
+    }
+
+    private fun updateStatus(newStatus: String) {
+        runOnUiThread { status.text = newStatus }
+    }
+
+    private fun updateLastPayload(newPayload: String) {
+        runOnUiThread { lastChirp.text = newPayload }
+    }
+
+    private fun stopSdk() {
+        val error = chirpConnect.stop()
+        if (error.code > 0) {
+            Log.e(TAG, "ConnectError: " + error.message)
+            return
+        }
+        sendPayloadBtn.alpha = .4f
+        sendPayloadBtn.isClickable = false
+        startStopSdkBtn.text = "Start Sdk"
+    }
+
+    private fun startSdk() {
+        val error = chirpConnect.start()
+        if (error.code > 0) {
+            Log.e(TAG, "ConnectError: " + error.message)
+            return
+        }
+        sendPayloadBtn.alpha = 1f
+        sendPayloadBtn.isClickable = true
+        startStopSdkBtn.text = "Stop Sdk"
+    }
+
+    private fun startStopSdk() {
+        /**
+         * Start or stop the SDK.
+         * Audio is only processed when the SDK is running.
+         */
+        startStopSdkBtnPressed = true
+        if (chirpConnect.getState() === ChirpConnectState.CHIRP_CONNECT_STATE_STOPPED ) {
+            startSdk()
+        } else {
+            stopSdk()
+        }
+    }
+
+    private fun sendPayload() {
+        /**
+         * A payload is a byte array dynamic size with a maximum size defined by the config string.
+         *
+         * Generate a random payload, and send it.
+         */
+        val payload = chirpConnect.randomPayload(0)     //size = 0 will generate a random payload size
+        val error = chirpConnect.send(payload)
+        if (error.code > 0) {
+            Log.e("ConnectError: ", error.message)
+        }
+    }
+}
